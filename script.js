390b705c40cb9405f14334e033dd4498fee3c1ed
Shashank  Pandey <shashankpandey2001@gmail.com>
2023-02-19 14:56:25 +0530

    codes2

--- script.js
+++ script.js
@@ -21,3 +21,36 @@ console.log(GetTheme);
 if (GetTheme === "LIGHT") {
   document.body.classList = "light-mode";
 }
+
+//Slides for Reviews
+let slides = document.querySelectorAll(".slide-container");
+let reviewIndex = 0;
+
+function next() {
+  slides[reviewIndex].classList.remove("active");
+  reviewIndex = (reviewIndex + 1) % slides.length;
+  slides[reviewIndex].classList.add("active");
+}
+function prev() {
+  slides[reviewIndex].classList.remove("active");
+  if (reviewIndex == 0) {
+    reviewIndex = slides.length;
+  }
+  reviewIndex = (reviewIndex - 1) % slides.length;
+  slides[reviewIndex].classList.add("active");
+}
+
+//Frequently Asked Questions//
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
